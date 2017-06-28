22dff6d24bddb5f14ff21af6a7b9c247ada9c657
argulworm <334145486@qq.com>
2017-06-28 23:42:48 +0800

    attr defs

--- battle/formula.js
+++ battle/formula.js
@@ -1,3 +1,22 @@
 /**
  * Created by argulworm on 6/14/17.
  */
+
+dt.formula = {
+    damage: function () {
+        
+    },
+    
+    _getPercent: function (pct, min, max) {
+        min = dt.suger.isUndefined(min) ? 0 : min;
+        max = dt.suger.isUndefined(max) ? 0 : max;
+        var ret = pct / 100.0;
+        if (ret < 0) return 0;
+        else if (ret > 1) return 1;
+        else return ret;
+    },
+    
+    _getBool: function (pct, rnd) {
+        return rnd.random01() <= this._getPercent(pct);
+    }
+};
