38eeb3744335637e6bd325b29fece893a68ef490
Argul Worm <334145486@qq.com>
2017-07-20 09:59:19 +0800

    trivial

--- before.js
+++ before.js
@@ -9,39 +9,16 @@ var dt = dt || {};
 dt.Class = function () {
 };
 var __g_initializing = false;
-var __g_fnTest = /xyz/.test(function () {
-    xyz;
-}) ? /\b_super\b/ : /.*/;
 dt.Class.extend = function (prop) {
-    var _super = this.prototype;
-
     // Instantiate a base class (but only create the instance,
     // don't run the init constructor)
     __g_initializing = true;
-    var prototype = Object.create(_super);
+    var prototype = Object.create(this.prototype);
     __g_initializing = false;
     // Copy the properties over onto the new prototype
     for (var name in prop) {
         // Check if we're overwriting an existing function
-        prototype[name] = typeof prop[name] == "function" &&
-        typeof _super[name] == "function" && __g_fnTest.test(prop[name]) ?
-            (function (name, fn) {
-                return function () {
-                    var tmp = this._super;
-
-                    // Add a new ._super() method that is the same method
-                    // but on the super-class
-                    this._super = _super[name];
-
-                    // The method only need to be bound temporarily, so we
-                    // remove it when we're done executing
-                    var ret = fn.apply(this, arguments);
-                    this._super = tmp;
-
-                    return ret;
-                };
-            })(name, prop[name]) :
-            prop[name];
+        prototype[name] = prop[name];
     }
 
     // The dummy class constructor
